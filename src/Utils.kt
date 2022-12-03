5d24bd493e305899d1839bf1b602258f10fddbca
Martin Lundberg <martin_o_lundberg@hotmail.com>
2022-12-03 10:17:37 +0100

    feat: add parent parameter to readInput()

--- src/Utils.kt
+++ src/Utils.kt
@@ -5,7 +5,7 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt")
+fun readInput(parent: String, name: String) = File(parent, "$name.txt")
     .readLines()
 
 /**
