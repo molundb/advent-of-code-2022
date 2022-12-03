031d07fa5a3cbe256e30813d6a9bb88ae7b8095f
Martin Lundberg <martin_o_lundberg@hotmail.com>
2022-12-03 11:46:03 +0100

    feat: solve Day02 part two

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -5,8 +5,8 @@ import readInput
 fun main () {
     val input = readInput(parent = "src/Day02", name = "Day02_input")
 
-    val totalScore = solvePartOne(input)
-    println(totalScore)
+    println(solvePartOne(input))
+    println(solvePartTwo(input))
 }
 
 private fun solvePartOne(input: List<String>): Int {
@@ -20,6 +20,26 @@ private fun solvePartOne(input: List<String>): Int {
         Pair("C X", 7),
         Pair("C Y", 2),
         Pair("C Z", 6),
+    )
+
+    var totalScore = 0
+    input.forEach {
+        totalScore += combinations[it] ?: 0
+    }
+    return totalScore
+}
+
+private fun solvePartTwo(input: List<String>): Int {
+    val combinations = mapOf(
+        Pair("A X", 3),
+        Pair("A Y", 4),
+        Pair("A Z", 8),
+        Pair("B X", 1),
+        Pair("B Y", 5),
+        Pair("B Z", 9),
+        Pair("C X", 2),
+        Pair("C Y", 6),
+        Pair("C Z", 7),
     )
 
     var totalScore = 0
