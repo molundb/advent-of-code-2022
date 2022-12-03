d72c2dce06e53e902d609fd41855197ebf22710c
Martin Lundberg <martin_o_lundberg@hotmail.com>
2022-12-03 14:01:25 +0100

    feat: solve Day03 part two

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -6,11 +6,50 @@ fun main() {
     val input = readInput(parent = "src/Day03", name = "Day03_input")
 
     println(solvePartOne(input))
+    println(solvePartTwo(input))
+}
+
+private fun solvePartTwo(input: List<String>): Int {
+    var sum = 0
+    var teamCount = 0
+    val charsInFirst = mutableSetOf<Char>()
+    val charsInFirstAndSecond = mutableSetOf<Char>()
+
+    input.forEach { rucksack ->
+        when (teamCount % 3) {
+            0 -> {
+                charsInFirst.clear()
+                charsInFirst.addAll(rucksack.toSet())
+            }
+
+            1 -> {
+                charsInFirstAndSecond.clear()
+
+                for (c in rucksack) {
+                    if (charsInFirst.contains(c)) {
+                        charsInFirstAndSecond.add(c)
+                    }
+                }
+            }
+
+            2 -> {
+                for (c in rucksack) {
+                    if (charsInFirstAndSecond.contains(c)) {
+                        sum += c.getPriority()
+                        break
+                    }
+                }
+            }
+
+        }
+        teamCount++
+    }
+    return sum
 }
 
 private fun solvePartOne(input: List<String>): Int {
     var sum = 0
-    input.forEach {rucksack ->
+    input.forEach { rucksack ->
         val charsInFirstHalf = mutableSetOf<Char>()
         for (i in rucksack.indices) {
             val c = rucksack[i]
