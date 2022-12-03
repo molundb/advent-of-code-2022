5f6522599053a4d9fa1ffd9aad2305f2b29fdc7a
Martin Lundberg <martin_o_lundberg@hotmail.com>
2022-12-03 11:23:26 +0100

    feat: solve Day01 part two

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -6,6 +6,11 @@ import kotlin.math.max
 fun main() {
     val input = readInput(parent = "src/Day01", name = "Day01_input")
 
+    println(solvePartOne(input))
+    println(solvePartTwo(input))
+}
+
+private fun solvePartOne(input: List<String>): Int {
     var max = 0
     var curr = 0
     input.forEach { foodItem ->
@@ -16,6 +21,30 @@ fun main() {
             curr += foodItem.toInt()
         }
     }
+    return max
+}
 
-    println(max)
+private fun solvePartTwo(input: List<String>): Int {
+    val topThreeCalories = IntArray(3)
+    var curr = 0
+    input.forEach { foodItem ->
+        if (foodItem.isEmpty()) {
+            for (i in 0..2) {
+                if (curr > topThreeCalories[i]) {
+                    if (i == 0) {
+                        topThreeCalories[2] = topThreeCalories[1]
+                        topThreeCalories[1] = topThreeCalories[0]
+                    } else if (i == 1) {
+                        topThreeCalories[2] = topThreeCalories[1]
+                    }
+                    topThreeCalories[i] = curr
+                    break
+                }
+            }
+            curr = 0
+        } else {
+            curr += foodItem.toInt()
+        }
+    }
+    return topThreeCalories.sum()
 }
