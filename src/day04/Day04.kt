bf52d80abe0bbd9d35dce9b9c4410fda703832a2
Martin Lundberg <martin_o_lundberg@hotmail.com>
2022-12-04 10:20:15 +0100

    feat: solve Day04 part two

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -6,10 +6,11 @@ fun main() {
     val input = readInput(parent = "src/day04", name = "Day04_input")
 
     println(solvePartOne(input))
+    println(solvePartTwo(input))
 }
 
 private fun solvePartOne(input: List<String>): Int {
-    var sumOverlap = 0
+    var sumCompleteOverlap = 0
     input.forEach {
         val pair = it.split(',')
 
@@ -24,8 +25,29 @@ private fun solvePartOne(input: List<String>): Int {
         if (startPairOne >= startPairTwo && endPairOne <= endPairTwo
             || startPairOne <= startPairTwo && endPairOne >= endPairTwo
         ) {
-            sumOverlap++
+            sumCompleteOverlap++
         }
     }
-    return sumOverlap
+    return sumCompleteOverlap
+}
+
+private fun solvePartTwo(input: List<String>): Int {
+    var sumPartialOverlap = 0
+    input.forEach {
+        val pair = it.split(',')
+
+        val startAndEndPairOne = pair[0].split('-')
+        val startAndEndPairTwo = pair[1].split('-')
+
+        val startPairOne = startAndEndPairOne[0].toInt()
+        val startPairTwo = startAndEndPairTwo[0].toInt()
+        val endPairOne = startAndEndPairOne[1].toInt()
+        val endPairTwo = startAndEndPairTwo[1].toInt()
+
+        if (!(endPairOne < startPairTwo || startPairOne > endPairTwo)
+        ) {
+            sumPartialOverlap++
+        }
+    }
+    return sumPartialOverlap
 }
