f3068db22fed74ee453a7ebaf1a6d46d72663358
Martin Lundberg <martin_o_lundberg@hotmail.com>
2022-12-06 22:48:50 +0100

    feat: fix Day06 part one and solve Day06 part two

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -6,23 +6,39 @@ fun main() {
     val input = readInput(parent = "src/day06", name = "Day06_input")
 
     println(solvePartOne(input))
+    println(solvePartTwo(input))
 }
 
-private fun solvePartOne(input: List<String>): Int {
-    var lastFour = mutableListOf<Char>()
-    var lastDuplicate = 0
+private fun solvePartOne(input: List<String>)= solve(input, 4)
+private fun solvePartTwo(input: List<String>) = solve(input, 14)
+
+private fun solve(input: List<String>, distinctCharacters: Int): Int {
+    var lastSeen = mutableListOf<Char>()
+    val numberOfEachLetter = mutableMapOf<Char, Int>()
 
     for ((i, c) in input[0].withIndex()) {
-        if (lastFour.contains(c)) {
-            lastDuplicate = i
-        } else if (i > 3 && i - lastDuplicate > 3) {
-            return i
+        numberOfEachLetter[c] = if (numberOfEachLetter.contains(c)) {
+            numberOfEachLetter[c]!! + 1
+        } else {
+            1
         }
 
-        if (lastFour.size < 4) {
-            lastFour.add(c)
+        if (numberOfEachLetter.size == distinctCharacters) {
+            return i + 1
+        }
+
+        if (lastSeen.size < distinctCharacters - 1) {
+            lastSeen.add(c)
         } else {
-            lastFour = (listOf(c) + lastFour.dropLast(1)).toMutableList()
+            val firstC = lastSeen.first()
+            val toReduce = numberOfEachLetter[firstC]!!
+            if (toReduce > 1) {
+                numberOfEachLetter[firstC] = toReduce - 1
+            } else {
+                numberOfEachLetter.remove(firstC)
+            }
+
+            lastSeen = (lastSeen.drop(1) + listOf(c)).toMutableList()
         }
     }
 
