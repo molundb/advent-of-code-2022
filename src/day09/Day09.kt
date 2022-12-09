584bfae539022852a2a26af1baa4107c1869e3f8
Martin Lundberg <martin_o_lundberg@hotmail.com>
2022-12-09 12:35:21 +0100

    feat: solve Day09 part two

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -7,6 +7,61 @@ fun main() {
     val input = readInput(parent = "src/day09", name = "Day09_input")
 
     println(solvePartOne(input))
+    println(solvePartTwo(input))
+}
+
+private fun solvePartTwo(input: List<String>): Int {
+    val startPoint = Point(0, 0)
+
+    val tailVisitedPoints = mutableSetOf(startPoint)
+
+    val points = MutableList(10) { startPoint.copy() }
+
+    for (l in input) {
+        val (direction, distance) = l.split(' ')
+
+        for (i in 0 until distance.toInt()) {
+            when (direction) {
+                "R" -> points[0].x++
+                "D" -> points[0].y++
+                "L" -> points[0].x--
+                "U" -> points[0].y--
+            }
+            updateTailPoints(points, tailVisitedPoints)
+
+        }
+    }
+    return tailVisitedPoints.size
+}
+
+private fun updateTailPoints(
+    points: MutableList<Point>,
+    tailVisitedPoints: MutableSet<Point>
+) {
+    var prevPoint = points[0]
+
+    for (j in 1 until points.size) {
+        if (notAdjacent(prevPoint, points[j])) {
+            if (prevPoint.x > points[j].x) {
+                points[j].x++
+            } else if (prevPoint.x < points[j].x) {
+                points[j].x--
+            }
+
+            if (prevPoint.y > points[j].y) {
+                points[j].y++
+            } else if (prevPoint.y < points[j].y) {
+                points[j].y--
+            }
+
+            if (j == points.lastIndex) {
+                tailVisitedPoints.add(Point(points[j].x, points[j].y))
+            }
+        } else {
+            break
+        }
+        prevPoint = points[j]
+    }
 }
 
 private fun solvePartOne(input: List<String>): Int {
