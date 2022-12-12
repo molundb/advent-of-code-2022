d52550777c4b54bcb000bcabad9c19fcd4072a1f
Martin Lundberg <martin_o_lundberg@hotmail.com>
2022-12-12 19:38:22 +0100

    feat: solve Day10 part two

--- src/day10/Day10.kt
+++ src/day10/Day10.kt
@@ -1,10 +1,52 @@
 package day10
 
 import readInput
+import kotlin.math.abs
 
 fun main() {
     val input = readInput(parent = "src/day10", name = "Day10_input")
 
+    println(solvePartOne(input))
+    println(solvePartTwo(input))
+}
+
+private fun solvePartTwo(input: List<String>): String {
+    var allLines = ""
+    var currLine = "#"
+    var c = 1
+    var x = 1
+
+    for (l in input) {
+        if (c % 40 == 0) {
+            allLines += "$currLine\n"
+            currLine = ""
+        }
+
+        currLine += draw(c, x)
+
+        if (l.startsWith("addx")) {
+            c++
+            if (c % 40 == 0) {
+                allLines += "$currLine\n"
+                currLine = ""
+            }
+            x += l.split(' ')[1].toInt()
+            currLine += draw(c, x)
+        }
+
+        c++
+    }
+
+    return allLines
+}
+
+private fun draw(c: Int, x: Int) = if (abs(c % 40 - x) <= 1) {
+    "#"
+} else {
+    "."
+}
+
+private fun solvePartOne(input: List<String>): Int {
     var signalStrengthSum = 0
     var c = 1
     var x = 1
@@ -20,9 +62,7 @@ fun main() {
 
         c++
     }
-
-    println(signalStrengthSum)
-
+    return signalStrengthSum
 }
 
 private fun calc(c: Int, x: Int) = if ((c - 20) % 40 == 0) {
