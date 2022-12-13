abf7ed9de76491ab3fb1f6cdc871daaea9c90b13
Martin Lundberg <martin_o_lundberg@hotmail.com>
2022-12-13 23:58:42 +0100

    feat: solve Day11 part two

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -6,15 +6,64 @@ fun main() {
     val input = readInput(parent = "src/day11", name = "Day11_input")
 
     println(solvePartOne(input))
+    println(solvePartTwo(input))
 }
 
-private fun solvePartOne(input: List<String>): Int {
+private fun solvePartTwo(input: List<String>): Long {
+    val monkeys = mutableListOf<Monkey>()
+
+    var divisibleMod = 1L
+    var i = 0
+    val monkeyInfoSize = 7
+    while (i < input.size) {
+        val items = input[i + 1].split(": ")[1].split(", ").map { it.toLong() }
+        val operation = input[i + 2].split(' ')[6]
+        val operationVal = input[i + 2].split(' ')[7]
+
+        val divisibleBy = input[i + 3].split(' ')[5].toInt()
+        divisibleMod *= divisibleBy
+
+        val ifTrue = input[i + 4].split(' ')[9].toInt()
+        val ifFalse = input[i + 5].split(' ')[9].toInt()
+
+        monkeys.add(
+            Monkey(
+                items = items.toMutableList(),
+                operation = getOperation(operation, operationVal),
+                test = Test(divisibleBy, ifTrue, ifFalse)
+            )
+        )
+
+        i += monkeyInfoSize
+    }
+
+
+    repeat(10000) {
+        for (m in monkeys) {
+            for (item in m.items) {
+                val new = m.operation(item) % divisibleMod
+                if (new % m.test.divisibleBy == 0L) {
+                    monkeys[m.test.ifTrue].items.add(new)
+                } else {
+                    monkeys[m.test.ifFalse].items.add(new)
+                }
+            }
+            m.inspections += m.items.size.toLong()
+            m.items.clear()
+        }
+    }
+
+    monkeys.sortByDescending { it.inspections }
+
+    return monkeys[0].inspections * monkeys[1].inspections
+}
+private fun solvePartOne(input: List<String>): Long {
     val monkeys = mutableListOf<Monkey>()
 
     var i = 0
     val monkeyInfoSize = 7
     while (i < input.size) {
-        val items = input[i + 1].split(": ")[1].split(", ").map { it.toInt() }
+        val items = input[i + 1].split(": ")[1].split(", ").map { it.toLong() }
         val operation = input[i + 2].split(' ')[6]
         val operationVal = input[i + 2].split(' ')[7]
 
@@ -39,7 +88,7 @@ private fun solvePartOne(input: List<String>): Int {
             for (item in m.items) {
                 m.inspections++
                 val new = m.operation(item) / 3
-                if (new % m.test.divisibleBy == 0) {
+                if (new % m.test.divisibleBy == 0L) {
                     monkeys[m.test.ifTrue].items.add(new)
                 } else {
                     monkeys[m.test.ifFalse].items.add(new)
@@ -59,36 +108,36 @@ private fun getOperation(operation: String, operationVal: String) =
         if (operationVal == "old") {
             add2()
         } else {
-            add(operationVal.toInt())
+            add(operationVal.toLong())
         }
     } else {
         if (operationVal == "old") {
             multiply2()
         } else {
-            multiply(operationVal.toInt())
+            multiply(operationVal.toLong())
         }
     }
 
-private fun add(x: Int): (Int) -> Int = { old: Int ->
+private fun add(x: Long): (Long) -> Long = { old: Long ->
     x + old
 }
 
-private fun add2(): (Int) -> Int = { old: Int ->
+private fun add2(): (Long) -> Long = { old: Long ->
     old + old
 }
 
-private fun multiply(x: Int): (Int) -> Int = { old: Int ->
+private fun multiply(x: Long): (Long) -> Long = { old: Long ->
     x * old
 }
 
-private fun multiply2(): (Int) -> Int = { old: Int ->
+private fun multiply2(): (Long) -> Long = { old: Long ->
     old * old
 }
 
 data class Monkey(
-    var inspections: Int = 0,
-    val items: MutableList<Int>,
-    val operation: (Int) -> Int,
+    var inspections: Long = 0,
+    val items: MutableList<Long>,
+    val operation: (Long) -> Long,
     val test: Test,
 )
 
